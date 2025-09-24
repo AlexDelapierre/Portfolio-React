1bcd463b95329aedc88828cf92d60d862da3d55a
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2025-09-24 18:45:18 +0200

    style: add text-secondary-color in index.css

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,7 @@ import "./About.css";
 function About() {
   return (
     <section id="about" className="about">
-      <h2>A propos de <span className='text-secondary-color'>moi</span></h2>
+      <h2>A propos <span className='text-secondary-color'>de moi</span></h2>
       <p>
         Développeur passionné, je crée des applications web modernes et performantes pour donner vie à vos projets.
       </p>
