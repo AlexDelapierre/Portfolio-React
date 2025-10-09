e71a5940c77ffe5a46c794fbe48a41937ca3ea73
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2025-10-09 17:11:35 +0200

    style: update skills card grid

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -3,13 +3,6 @@ import {FaLaptopCode, FaReact, FaDatabase, FaMobileAlt, FaHtml5, FaPalette, FaTo
 
 const skillsData = [
     {
-      icon: FaLaptopCode,
-      title: "Frontend",
-      tags: ["JavaScript", "React", "Twig", "Responsive Design", "Mobile First"],
-      backgroundColor: "#3b347a",
-      color: "#c27aff"
-    },
-    {
       icon: FaDatabase,
       title: "Backend",
       tags: ["TypeScript", "NodeJS", "Express", "PHP", "Symfony", "SQL", "MongoDB", "Doctrine", "API REST"],
@@ -17,20 +10,25 @@ const skillsData = [
       color: "#05df72"
     },
     {
-      icon: FaPalette,
-      title: "Intégration Web",
-      tags: ["HTML", "CSS", "Bootstrap"],
+      icon: FaLaptopCode,
+      title: "Frontend",
+      tags: ["JavaScript", "React", "Twig", "Responsive Design", "Mobile First"],
       backgroundColor: "#484946",
       color: "#fdc700"
-      // backgroundColor: "#3b347a",
-      // color: "#c27aff"
     },
     {
       icon: FaTools,
       title: "Outils",
       tags: ["Git/GitHub", "VS Code", "Vite", "Postman", "AdobeXD", "Figma"],
-      backgroundColor: "#4a2f51",
-      color: "#ff6467"
+      backgroundColor: "#3b347a",
+      color: "#c27aff"
+    },
+    {
+      icon: FaPalette,
+      title: "Intégration Web",
+      tags: ["HTML", "CSS", "Bootstrap"],
+      backgroundColor: "#213f7a",
+      color: "#50a2ff"
     }
   ];
 
